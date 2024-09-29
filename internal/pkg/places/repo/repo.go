7b017045d4b81affa73b6b37f6f78809383b34ef
timurIsaevIY <isaevtimur2016@gmail.com>
2024-09-29 13:40:54 +0300

    errors fixed

--- internal/pkg/places/repo/repo.go
+++ internal/pkg/places/repo/repo.go
@@ -20,12 +20,10 @@ var jsonFileData []byte
 
 func (r *PlaceRepository) GetPlaces(ctx context.Context) ([]models.Place, error) {
 	images := make([]models.Place, 0)
-	log.Println("1")
 	err := json.Unmarshal(jsonFileData, &images)
 	if err != nil {
 		log.Println(err)
 		return nil, err
 	}
-	log.Println(images)
 	return images, nil
 }
