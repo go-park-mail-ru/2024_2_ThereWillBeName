bcd9aeb2250d98358818e39937860d0136f5d252
mevain <xen.bardikina@yandex.ru>
2024-10-01 18:29:11 +0300

    added middleware only to handler

--- cmd/main.go
+++ cmd/main.go
@@ -68,10 +68,7 @@ func main() {
 	auth.HandleFunc("/login", h.Login).Methods(http.MethodPost)
 	auth.HandleFunc("/logout", h.Logout).Methods(http.MethodPost)
 	users := auth.PathPrefix("/users").Subrouter()
-	users.Use(func(next http.Handler) http.Handler {
-		return middleware.MiddlewareAuth(jwtHandler, http.HandlerFunc(h.CurrentUser))
-	})
-	users.Handle("/me", http.HandlerFunc(h.CurrentUser)).Methods(http.MethodGet)
+	users.Handle("/me", middleware.MiddlewareAuth(jwtHandler, http.HandlerFunc(h.CurrentUser))).Methods(http.MethodGet)
 	places := r.PathPrefix("/places").Subrouter()
 	places.HandleFunc("", handler.GetPlaceHandler).Methods(http.MethodGet)
 	srv := &http.Server{
