191110deef6346c1d2d98b07e48f8dd1667b717d
AnnHarvard <dana.shakleina@yandex.ru>
2024-10-02 11:55:03 +0300

    fixed case with data==nil

--- internal/pkg/httpresponses/responses.go
+++ internal/pkg/httpresponses/responses.go
@@ -8,6 +8,12 @@ import (
 func SendJSONResponse(w http.ResponseWriter, data interface{}, status int) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
+
+	if data == nil {
+		http.Error(w, "Нет данных для отправки", http.StatusNoContent)
+		return
+	}
+
 	if err := json.NewEncoder(w).Encode(data); err != nil {
 		http.Error(w, "Не удалось преобразовать в json", http.StatusInternalServerError)
 	}
