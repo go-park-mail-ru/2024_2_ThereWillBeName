c375292ddf5525db3ef7d993d8d15d0cf3d5dadf
AnnHarvard <dana.shakleina@yandex.ru>
2024-10-02 12:51:31 +0300

    fixed mistakes

--- internal/pkg/httpresponses/responses.go
+++ internal/pkg/httpresponses/responses.go
@@ -6,14 +6,13 @@ import (
 )
 
 func SendJSONResponse(w http.ResponseWriter, data interface{}, status int) {
-	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 
 	if data == nil {
-		http.Error(w, "Нет данных для отправки", http.StatusNoContent)
 		return
 	}
 
+	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(data); err != nil {
 		http.Error(w, "Не удалось преобразовать в json", http.StatusInternalServerError)
 	}
