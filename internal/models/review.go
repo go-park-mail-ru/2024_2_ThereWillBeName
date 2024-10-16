e5ba42b68c292b95ed5031479694de84a672d94c
AnnHarvard <dana.shakleina@yandex.ru>
2024-10-16 11:55:47 +0300

    removed db tags

--- internal/models/review.go
+++ internal/models/review.go
@@ -3,10 +3,10 @@ package models
 import "time"
 
 type Review struct {
-	ID         uint      `json:"id" db:"id"`
-	UserID     uint      `json:"user_id" db:"user_id"`
-	PlaceID    uint      `json:"place_id" db:"place_id"`
-	Rating     int       `json:"rating" db:"rating"`
-	ReviewText string    `json:"review_text" db:"review_text"`
-	CreatedAt  time.Time `json:"created_at" db:"created_at"`
+	ID         uint      `json:"id"`
+	UserID     uint      `json:"user_id"`
+	PlaceID    uint      `json:"place_id"`
+	Rating     int       `json:"rating"`
+	ReviewText string    `json:"review_text"`
+	CreatedAt  time.Time `json:"created_at"`
 }
