c24a3298b67fcf29e76fca466ef0f6da52189e0b
AnnHarvard <dana.shakleina@yandex.ru>
2024-11-23 13:15:50 +0300

    fix

--- internal/models/survey.go
+++ internal/models/survey.go
@@ -1,27 +1,26 @@
 package models
 
 type Survey struct {
-	ID         uint   `json:"id"`
+	Id         uint   `json:"id"`
 	SurveyText string `json:"survey_text"`
 	MaxRating  int    `json:"max_rating"`
 }
 
 type SurveyResponse struct {
-	SurveyID uint `json:"survey_id"`
-	UserID   uint `json:"user_id"`
+	SurveyId uint `json:"survey_id"`
+	UserId   uint `json:"user_id"`
 	Rating   int  `json:"rating"`
 }
 
 type SurveyStatsBySurvey struct {
-	SurveyID     uint        `json:"survey_id"`
+	SurveyId     uint        `json:"survey_id"`
 	SurveyText   string      `json:"survey_text"`
 	AvgRating    float64     `json:"avg_rating"`
 	RatingsCount map[int]int `json:"ratings_count"`
 }
 
-// Статистика по ID пользователя
 type UserSurveyStats struct {
-	SurveyID   uint   `json:"survey_id"`
+	SurveyId   uint   `json:"survey_id"`
 	SurveyText string `json:"survey_text"`
 	Answered   bool   `json:"answered"`
 }
--- internal/pkg/survey/interfaces.go
+++ internal/pkg/survey/interfaces.go
@@ -6,15 +6,15 @@ import (
 )
 
 type SurveysUsecase interface {
-	GetSurveyById(ctx context.Context, surveyID uint) (models.Survey, error)
+	GetSurveyById(ctx context.Context, surveyId uint) (models.Survey, error)
 	CreateSurveyResponse(ctx context.Context, response models.SurveyResponse) error
-	GetSurveyStatsBySurveyId(ctx context.Context, surveyID uint) (models.SurveyStatsBySurvey, error)
-	GetSurveyStatsByUserId(ctx context.Context, userID uint) ([]models.UserSurveyStats, error)
+	GetSurveyStatsBySurveyId(ctx context.Context, surveyId uint) (models.SurveyStatsBySurvey, error)
+	GetSurveyStatsByUserId(ctx context.Context, userId uint) ([]models.UserSurveyStats, error)
 }
 
 type SurveysRepo interface {
-	GetSurveyById(ctx context.Context, surveyID uint) (models.Survey, error)
+	GetSurveyById(ctx context.Context, surveyId uint) (models.Survey, error)
 	CreateSurveyResponse(ctx context.Context, response models.SurveyResponse) error
-	GetSurveyStatsBySurveyId(ctx context.Context, surveyID uint) (models.SurveyStatsBySurvey, error)
-	GetSurveyStatsByUserId(ctx context.Context, userID uint) ([]models.UserSurveyStats, error)
+	GetSurveyStatsBySurveyId(ctx context.Context, surveyId uint) (models.SurveyStatsBySurvey, error)
+	GetSurveyStatsByUserId(ctx context.Context, userId uint) ([]models.UserSurveyStats, error)
 }
