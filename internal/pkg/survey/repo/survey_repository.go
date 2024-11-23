755403ef67c79028efc6699ae98242b73e34c1ec
mevain <xen.bardikina@yandex.ru>
2024-11-23 15:05:50 +0300

    finished repo

--- internal/pkg/survey/repo/survey_repository.go
+++ internal/pkg/survey/repo/survey_repository.go
@@ -7,8 +7,6 @@ import (
 	_ "embed"
 	"errors"
 	"fmt"
-
-	"github.com/lib/pq"
 )
 
 type SurveyRepository struct {
@@ -41,5 +39,64 @@ func (r *SurveyRepository) CreateSurveyResponse(ctx context.Context, response mo
 }
 
 func (r *SurveyRepository) GetSurveyStatsBySurveyId(ctx context.Context, surveyId uint) (models.SurveyStatsBySurvey, error) {
-	query = `SELECT `
+	query := `SELECT 
+    	s.id AS survey_id,
+    	s.review_text,
+    	us.rating,
+    	COUNT(us.user_id) AS count_of_users
+	FROM 
+    	survey s
+	LEFT JOIN 
+    	user_survey us ON s.id = us.survey_id
+	WHERE 
+		s.id = $1
+	GROUP BY 
+		s.id, s.review_text, us.rating
+	ORDER BY 
+		s.id, us.rating; `
+
+	row := r.db.QueryRowContext(ctx, query, surveyId)
+	var surveyStats models.SurveyStatsBySurvey
+	err := row.Scan(&surveyStats.SurveyId, &surveyStats.SurveyText, &surveyStats.AvgRating, &surveyStats.RatingsCount)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return models.SurveyStatsBySurvey{}, fmt.Errorf("trip not found: %w", models.ErrNotFound)
+		}
+		return models.SurveyStatsBySurvey{}, fmt.Errorf("failed to scan survey stats row: %w", models.ErrInternal)
+	}
+	return surveyStats, nil
+}
+
+func (r *SurveyRepository) GetSurveyStatsByUserId(ctx context.Context, userId uint) ([]models.UserSurveyStats, error) {
+	query := `SELECT 
+    s.id AS survey_id,
+    s.review_text,
+    CASE 
+        WHEN us.user_id IS NOT NULL THEN TRUE
+        ELSE FALSE
+    END AS participated
+FROM 
+    survey s
+LEFT JOIN 
+    user_survey us ON s.id = us.survey_id AND us.user_id = 1
+ORDER BY 
+    s.id;`
+	rows, err := r.db.QueryContext(ctx, query, userId)
+	if err != nil {
+		return nil, fmt.Errorf("failed to retrieve user stats: %w", models.ErrInternal)
+	}
+	defer rows.Close()
+	var userStats []models.UserSurveyStats
+	for rows.Next() {
+		var userStat models.UserSurveyStats
+		if err := rows.Scan(&userStat.SurveyId, &userStat.SurveyText, &userStat.Answered); err != nil {
+			return nil, fmt.Errorf("failed to scan user stat row: %w", models.ErrInternal)
+		}
+		userStats = append(userStats, userStat)
+	}
+	if len(userStats) == 0 {
+		return nil, fmt.Errorf("no user stats found: %w", models.ErrNotFound)
+	}
+
+	return userStats, nil
 }
