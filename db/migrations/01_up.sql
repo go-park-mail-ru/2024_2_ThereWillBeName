58d7b398df7717efd2a85cc3e4f6ee81fa1cf71c
timurIsaevIY <isaevtimur2016@gmail.com>
2024-11-23 16:06:02 +0300

    SCHEME BD CHANGE

--- db/migrations/01_up.sql
+++ db/migrations/01_up.sql
@@ -100,7 +100,7 @@ CREATE TABLE IF NOT EXISTS trip_place ( --таблица для сопостав
 CREATE TABLE IF NOT EXISTS survey (
     id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     review_text TEXT DEFAULT '',
-    max_rating INT NOT NULL CHECK (max_rating > 0) DEFAULT 1,
+    max_rating INT NOT NULL CHECK (max_rating > 0) DEFAULT 1
 );
 
 CREATE TABLE IF NOT EXISTS user_survey (
