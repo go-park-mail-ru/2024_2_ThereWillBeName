076fbb61aa8ef8f53e88ef80ca32edf68ef4d069
AnnHarvard <dana.shakleina@yandex.ru>
2024-11-23 14:46:43 +0300

    started usecase for surveys

--- internal/pkg/survey/usecase/survey_usecase.go
+++ internal/pkg/survey/usecase/survey_usecase.go
@@ -1,1 +1,33 @@
 package usecase
+
+import (
+	"2024_2_ThereWillBeName/internal/models"
+	"2024_2_ThereWillBeName/internal/pkg/survey"
+	"context"
+)
+
+type SurveysUseCaseImpl struct {
+	surveyRepo survey.SurveysRepo
+}
+
+func NewSurveysUsecase(repo survey.SurveysRepo) *SurveysUseCaseImpl {
+	return &SurveysUseCaseImpl{
+		surveyRepo: repo,
+	}
+}
+
+func (u *SurveysUseCaseImpl) GetSurveyById(ctx context.Context, surveyId uint) (models.Survey, error) {
+
+}
+
+func (u *SurveysUseCaseImpl) CreateSurveyResponse(ctx context.Context, surveyId uint) (models.Survey, error) {
+
+}
+
+func (u *SurveysUseCaseImpl) GetSurveyStatsBySurveyId(ctx context.Context, surveyId uint) (models.Survey, error) {
+
+}
+
+func (u *SurveysUseCaseImpl) GetSurveyStatsByUserId(ctx context.Context, surveyId uint) (models.Survey, error) {
+
+}
